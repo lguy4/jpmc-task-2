6910eb14b98c992e407389f8efd6642ab2ced3c5
lguy4 <lendeldeguia@gmail.com>
2023-02-26 20:47:24 -0600

    finished task

--- server.py
+++ server.py
@@ -217,8 +217,8 @@ def get(req_handler, routes):
                 req_handler.wfile.write(bytes(data, encoding='utf-8'))
                 return
 
-
-def run(routes, host='0.0.0.0', port=8080):
+# changed port to 10648
+def run(routes, host='0.0.0.0', port=10648):
     """ Runs a class as a server whose methods have been decorated with
         @route.
     """
@@ -234,7 +234,7 @@ def run(routes, host='0.0.0.0', port=8080):
     thread = threading.Thread(target=server.serve_forever)
     thread.daemon = True
     thread.start()
-    print('HTTP server started on port 8080')
+    print('HTTP server started on port 10648')
     while True:
         from time import sleep
         sleep(1)
--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -15,7 +15,8 @@ export interface ServerRespond {
 
 class DataStreamer {
   // The url where datafeed server is listening
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  // changed port to 10648
+  static API_URL: string = 'http://localhost:10648/query?id=1';
 
   /**
    * Send request to the datafeed server and executes callback function on success
